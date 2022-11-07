0191fdcad8ab6d228abc203971818692a23b622c
Ryan <ryan.eliopoulos@gmail.com>
2022-11-07 06:30:23 -0800

    +improvements
    When an offending URL is detected the extension now redirects to the
    extension page rather opening in a new tab. The entry in the tab's
    navigation history is then overwritten with the extension page so that
    the back button behavior doesn't just loop.
    
    The extension page has been made slightly prettier. Now includes the
    offending URL.
    
    The background script now sets an onStart callback, so hopefully the
    background scripts will now load the filters on browser start rather
    than requiring the browser_action UI to 'save changes'. Remains to be
    seen. Will require Mozilla sign off to make the extension persistent.

--- extension_page/extension_page.js
+++ extension_page/extension_page.js
@@ -1,8 +1,20 @@
+/*
+  The URL filter redirects the tab to the extension page with
+  the offending url stored in a raw string query parameter.
+
+  The filter avoids triggering again because URIs beginning with 'moz-extension' are ignored.
+  However, other websites including an offending pattern in their URL will still trigger.
+  Using the beginning line anchor in the patterns is ideal.
+ */
 
 let searchParams = new URLSearchParams(window.location.search)
 let url = searchParams.get('url')
 
+// Updating page to display the offending URL
+let p = document.getElementById('url-p')
+p.textContent = `${url}`
 
+// Registering override mechanism to the override button
 let ob = document.getElementById('override-button')
 ob.onclick = ()=>{
   browser.tabs.getCurrent()
@@ -20,5 +32,4 @@ ob.onclick = ()=>{
     .catch(error => {
       console.error(`Error getting current tab: ${error}`)
     })
-
 }
